c94fb6dd9af013ae188f285c9a27d54127a3546c
MelissaPerez111 <melissaperez2019@u.northwestern.edu>
2017-11-04 14:55:40 -0500

    code from my first app
    i'm working on adding the duck picture to a button

---
+++ My_First_App/app/src/main/java/com/example/melissa/my_first_app/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.melissa.my_first_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.melissa.my_first_app.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the SEND button **/
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    ImageView imageView = (ImageView) findViewById(R.id.myImageView);
+    imageView.setOnClickListener(new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(ThisActivity.this, MyOtherActivity.class);
+            startActivity(intent);
+        }
+    });
+}
